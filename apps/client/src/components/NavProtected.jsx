098dc67d1d763eae006b072949da5ac7b8998663
Justen Manni <mannijusten@gmail.com>
2022-10-11 22:19:43 +0800

    Add new images for Dev Page

--- apps/client/src/components/NavProtected.jsx
+++ apps/client/src/components/NavProtected.jsx
@@ -1,7 +1,7 @@
 import { Link, useNavigate } from "react-router-dom";
 
 function NavProtected({ setToken }) {
-  const navigate = useNavigate();
+  // const navigate = useNavigate();
   const handleLogout = () => {
     console.log("logout");
     setToken(0);
