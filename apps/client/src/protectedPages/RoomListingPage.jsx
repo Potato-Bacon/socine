aceb69accff37eeae49490bf38ec5168089859dc
Justen Manni <mannijusten@gmail.com>
2022-10-15 20:51:32 +0800

    Add new Room Listing Page with CSS updates

--- apps/client/src/protectedPages/RoomListingPage.jsx
+++ apps/client/src/protectedPages/RoomListingPage.jsx
@@ -1,9 +1,14 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
+import { Link, useNavigate, Navigate } from "react-router-dom";
 import Img from "react-cool-img";
 
 const roomListingURL = "/api/roomlistings";
 
 function RoomListingPage({ username, token }) {
+  const navigate = useNavigate();
+  const [roomListing, setRoomListing] = useState([]);
+
+  // fetch room listings
   useEffect(() => {
     fetch(roomListingURL, {
       headers: {
@@ -12,8 +17,9 @@ function RoomListingPage({ username, token }) {
       },
     })
       .then((response) => response.json())
-      .then((data) => console.log(data));
+      .then((data) => setRoomListing(data));
   }, []);
+
   return (
     <>
       <div>
@@ -31,139 +37,80 @@ function RoomListingPage({ username, token }) {
             </div>
 
             <div className="grid grid-cols-1 gap-8 mt-8 md:mt-16 md:grid-cols-2 xl:grid-cols-3">
-              <div>
-                <div className="relative">
-                  <Img
-                    loading="lazy"
-                    className="object-cover object-center w-full h-64 rounded-lg lg:h-80"
-                    src="https://images.pexels.com/photos/2102587/pexels-photo-2102587.jpeg"
-                    alt="home01"
-                  />
+              {roomListing.map((rl) => (
+                <>
+                  <div key={rl._id}>
+                    <div className="relative">
+                      <Img
+                        loading="lazy"
+                        className="object-cover object-center w-full h-64 rounded-lg lg:h-80"
+                        src={rl?.listingPic}
+                        alt="listingPic"
+                      />
 
-                  <div className="absolute bottom-0 flex p-3 bg-white dark:bg-gray-900 ">
-                    <Img
-                      className="object-cover object-center w-10 h-10 rounded-full"
-                      src="https://images.pexels.com/photos/2102587/pexels-photo-2102587.jpeg"
-                      alt="home02"
-                    />
+                      <div className="absolute bottom-0 flex p-3 bg-white dark:bg-gray-900 ">
+                        <Img
+                          className="object-cover object-center w-10 h-10 rounded-full"
+                          src="https://i.picsum.photos/id/1005/5760/3840.jpg?hmac=2acSJCOwz9q_dKtDZdSB-OIK1HUcwBeXco_RMMTUgfY"
+                          alt="listingPic"
+                        />
 
-                    <div className="mx-4">
-                      <h1 className="text-sm text-gray-700 dark:text-gray-200">
-                        Tom Hank
-                      </h1>
-                      <p className="text-sm text-gray-500 dark:text-gray-400">
-                        Creative Director
-                      </p>
+                        <div className="mx-4">
+                          <h1 className="text-sm text-gray-700 dark:text-gray-200">
+                            {rl?.name}
+                          </h1>
+                          <p className="text-sm text-gray-500 dark:text-gray-400">
+                            Creative Director
+                          </p>
+                        </div>
+                      </div>
                     </div>
-                  </div>
-                </div>
 
-                <h1 className="mt-6 text-xl font-semibold text-gray-800 dark:text-white">
-                  Spain inspired home for rent
-                </h1>
+                    <h1 className="mt-6 text-xl font-semibold text-gray-800 dark:text-white">
+                      {rl?.title}
+                    </h1>
+                    <p className="mt-1 text-l font-semibold text-gray-800 dark:text-white">
+                      {rl?.town}
+                    </p>
+                    <h1 className="mt-1 text-xl font-semibold text-gray-800 dark:text-white">
+                      ${rl?.rentPerMonth}{" "}
+                      <span className="text-xs">/month</span>
+                    </h1>
 
-                <hr className="w-32 my-6 text-blue-500" />
-
-                <p className="text-sm text-gray-500 dark:text-gray-400">
-                  Its hard to not miss this lovely home located at the hearts of
-                  Orchard Road.
-                </p>
-
-                <a
-                  href="#"
-                  className="inline-block mt-4 text-blue-500 underline hover:text-blue-400"
-                >
-                  Find Out More
-                </a>
-              </div>
-
-              <div>
-                <div className="relative">
-                  <Img
-                    className="object-cover object-center w-full h-64 rounded-lg lg:h-80"
-                    src="https://images.pexels.com/photos/731082/pexels-photo-731082.jpeg"
-                    alt=""
-                  />
-
-                  <div className="absolute bottom-0 flex p-3 bg-white dark:bg-gray-900 ">
-                    <Img
-                      className="object-cover object-center w-10 h-10 rounded-full"
-                      src="https://images.unsplash.com/photo-1535713875002-d1d0cf377fde?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=880&q=80"
-                      alt=""
-                    />
-
-                    <div className="mx-4">
-                      <h1 className="text-sm text-gray-700 dark:text-gray-200">
-                        arthur melo
-                      </h1>
-                      <p className="text-sm text-gray-500 dark:text-gray-400">
-                        Creative Director
-                      </p>
+                    <div className="flex-row justify-items-start my-3">
+                      <span className="bg-indigo-100 text-indigo-800 text-xs font-semibold mr-2 px-2.5 py-0.5 rounded dark:bg-indigo-200 dark:text-indigo-900">
+                        {rl?.wholeUnitOrRoomOnly}
+                      </span>
+                      <span className="bg-indigo-100 text-indigo-800 text-xs font-semibold mr-2 px-2.5 py-0.5 rounded dark:bg-indigo-200 dark:text-indigo-900">
+                        {rl?.apartmentType}
+                      </span>
+                      <span className="bg-indigo-100 text-indigo-800 text-xs font-semibold mr-2 px-2.5 py-0.5 rounded dark:bg-indigo-200 dark:text-indigo-900">
+                        {rl?.apartmentRoomTypes}
+                      </span>
                     </div>
-                  </div>
-                </div>
-
-                <h1 className="mt-6 text-xl font-semibold text-gray-800 dark:text-white">
-                  Wooden cabin located at East Coast area
-                </h1>
-
-                <hr className="w-32 my-6 text-blue-500" />
-
-                <p className="text-sm text-gray-500 dark:text-gray-400">
-                  One of the best preserved wooden cabin overlooking the sea.
-                </p>
-
-                <a
-                  href="#"
-                  className="inline-block mt-4 text-blue-500 underline hover:text-blue-400"
-                >
-                  Find Out More
-                </a>
-              </div>
-
-              <div>
-                <div className="relative">
-                  <Img
-                    className="object-cover object-center w-full h-64 rounded-lg lg:h-80"
-                    src="https://images.pexels.com/photos/2631746/pexels-photo-2631746.jpeg"
-                    alt=""
-                  />
-
-                  <div className="absolute bottom-0 flex p-3 bg-white dark:bg-gray-900 ">
-                    <Img
-                      className="object-cover object-center w-10 h-10 rounded-full"
-                      src="https://images.unsplash.com/photo-1531590878845-12627191e687?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=764&q=80"
-                      alt=""
-                    />
-
-                    <div className="mx-4">
-                      <h1 className="text-sm text-gray-700 dark:text-gray-200">
-                        Amelia. Anderson
-                      </h1>
-                      <p className="text-sm text-gray-500 dark:text-gray-400">
-                        Lead Developer
-                      </p>
+                    <div className="flex-row justify-items-start my-3">
+                      <span className="bg-yellow-100 text-yellow-800 text-xs font-semibold mr-2 px-2.5 py-0.5 rounded dark:bg-yellow-200 dark:text-yellow-900">
+                        {rl?.listingTags?.[0]}
+                      </span>
+                      <span className="bg-yellow-100 text-yellow-800 text-xs font-semibold mr-2 px-2.5 py-0.5 rounded dark:bg-yellow-200 dark:text-yellow-900">
+                        {rl?.mrt} Mrt Station
+                      </span>
                     </div>
+                    <hr className="w-32 my-6 text-blue-500" />
+
+                    <p className="text-sm text-gray-500 dark:text-gray-400">
+                      {rl?.shortDescription}
+                    </p>
+
+                    <a
+                      href="#"
+                      className="inline-block mt-4 text-blue-500 underline hover:text-blue-400"
+                    >
+                      Find Out More
+                    </a>
                   </div>
-                </div>
-
-                <h1 className="mt-6 text-xl font-semibold text-gray-800 dark:text-white">
-                  Spacious Master Bedroom for rent
-                </h1>
-
-                <hr className="w-32 my-6 text-blue-500" />
-
-                <p className="text-sm text-gray-500 dark:text-gray-400">
-                  Looking for a couple to take over this room, before christmas.
-                </p>
-
-                <a
-                  href="#"
-                  className="inline-block mt-4 text-blue-500 underline hover:text-blue-400"
-                >
-                  Find Out More
-                </a>
-              </div>
+                </>
+              ))}
             </div>
           </div>
         </section>
