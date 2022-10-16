179097b42061b61a5226e52a835fa9bdc57afde1
Justen Manni <mannijusten@gmail.com>
2022-10-16 16:04:10 +0800

    Update SearchBarUserListing

--- apps/client/src/protectedComponents/SearchBarUserListing.jsx
+++ apps/client/src/protectedComponents/SearchBarUserListing.jsx
@@ -149,7 +149,7 @@ function SearchBarUserListing({ setUserListing, userListing }) {
               placeholder="Maximum Budget"
             />
           </div>
-          <div className="flex-row flex justify-items-start justify-center">
+          <div className="flex-row flex justify-items-start justify-center ">
             {/* Interests */}
             <label
               htmlFor="interests"
@@ -160,7 +160,7 @@ function SearchBarUserListing({ setUserListing, userListing }) {
             <select
               multiple
               name="interests"
-              className="block p-4 pl-10 w-72 text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 mt-5"
+              className="block p-4 pl-10 w-72 text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 mt-5 mx-2"
               placeholder="Maximum Budget"
             >
               {interests?.data?.map((interest) => (
@@ -169,9 +169,8 @@ function SearchBarUserListing({ setUserListing, userListing }) {
               ))}
               {/* <option value={"634441e31953bd9d609ef3c0"}>Basketball</option> */}
             </select>
-          </div>
 
-          <div className="flex-row flex justify-items-start justify-center">
+            {/* Listing Tags */}
             <label
               htmlFor="userListingTag"
               className="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-gray-300"
@@ -193,14 +192,14 @@ function SearchBarUserListing({ setUserListing, userListing }) {
             </select>
           </div>
           {/* Submit Search Button */}
-          <div className="flex-row flex justify-items-center justify-center my-5">
+          {/* <div className="flex-row flex justify-items-center justify-center my-5">
             <button
               type="submit"
               className="text-white right-2.5 bottom-2.5 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
             >
               Search
             </button>
-          </div>
+          </div> */}
         </form>
       </div>
     </>
